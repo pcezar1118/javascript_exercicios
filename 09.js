3068c13154dba7e934099fe4a5b6a92a54e6ae57
pcezar1118 <pcezar1118@gmail.com>
2022-10-15 19:34:01 -0300

    09

---
+++ 09.js
@@ -0,0 +1,26 @@
+function controllerAcesso() {
+    var senha = document.getElementById("senha").value;
+
+    var restornoValidacao = validaAcesso(senha);
+
+    if (restornoValidacao) {
+        window.location.href = "http://google.com.br";
+    }
+    else {
+        saidaTexto("Senha invalida!");
+    }
+
+}
+
+function validaAcesso(senha = "") {
+    var global_senhaValida = "1234"; //vem do banco de dados
+    if (senha == global_senhaValida) {
+        return true;//"ACESSO PERMITIDO";
+    } else {
+        return false; //"ACESSO NEGADO";
+    }
+}
+
+function saidaTexto(texto) {
+    document.getElementById("saidaTexto").innerHTML = texto;
+}
