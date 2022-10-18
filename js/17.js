dec724cc7c5776a8112d74e4d8dd3438918029a4
pcezar1118 <pcezar1118@gmail.com>
2022-10-18 19:21:22 -0200

    17

---
+++ js/17.js
@@ -0,0 +1,25 @@
+function arrOperacao() {
+
+var valor1 = 1;
+var valor2 = 2;
+var valor3 = 3;
+var valor4 = 4;
+
+return arrOperacao;
+
+}
+
+function calculoResultado(valor1, valor2, valor3, valor4){
+
+    let resultado = a * b;
+    return resultado;
+}
+
+
+
+
+
+
+function saidaDados(texto) {
+    document.getElementById("saidaTexto").innerHTML = texto;
+}
