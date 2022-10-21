9b451ab7974bc5e7338cae4532bf713b81de2693
pcezar1118 <pcezar1118@gmail.com>
2022-10-21 18:12:53 -0200

    53

---
+++ js/53.js
@@ -0,0 +1,27 @@
+function calculoMedia() {
+
+const media = 5;
+var arrNotas = ["nota1", "nota2", "nota3", "nota4"];
+var soma = nota1 + nota2 + nota3 + nota4;
+const resultado = soma / 4;
+}
+
+function soma(nota1, nota2, nota3, nota4) {
+
+if(resultado >= 20) {
+
+return Aprovado;
+
+} else {
+    return Reprovado;
+}
+}
+
+
+
+
+
+
+function saidaDados(texto) {
+    document.getElementById("saidaTexto").innerHTML = texto;
+}
